22cd3f0513524811aa767384ae1667b1c74a4585
JakubJS <elodonskuba@gmail.com>
2018-01-15 21:39:18 +0100

    Basic security config + Spring Securituy dependencies

---
+++ src/main/java/itforum/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package itforum.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	public DataSource dataSource;
+	
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			    /*.formLogin()
+			    	.loginPage("/login")
+				.and()*/
+			.httpBasic()
+				.realmName("ITForum")
+			.and()
+			.logout()
+				.logoutSuccessUrl("/")
+			.and()	
+			/*.rememberMe()
+				.tokenValiditySeconds(10)
+			.and()*/
+			.authorizeRequests() 
+					.antMatchers("/rules").hasAnyAuthority("USER", "ADMIN")
+					.anyRequest().permitAll()
+			.and()		
+				.sessionManagement().sessionFixation().none()
+			.and()
+				.csrf().disable();
+		} 
+	
+	@Autowired
+	public void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+	  /*auth.inMemoryAuthentication()
+	  	.withUser("user").password("123456").roles("USER")
+	  	.and()
+	  	.withUser("admin").password("admin").roles("ADMIN")
+	  	.and()
+	  	.withUser("dba").password("123456").roles("DBA");*/
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery(
+					"SELECT nick, password, enabled FROM user WHERE nick=?")
+			.authoritiesByUsernameQuery(
+					"SELECT nick, role FROM user WHERE nick=?");
+	}
+}
---
+++ src/main/java/itforum/config/SecurityWebInitializer.java
@@ -0,0 +1,7 @@
+package itforum.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
