0aaf780db3fff80bbe756e8f16dad400d7f6bcd2
JakubJS <elodonskuba@gmail.com>
2018-01-22 23:17:32 +0100

    Changes in logout and login config

--- src/main/java/itforum/web/LoginController.java
+++ src/main/java/itforum/web/LoginController.java
@@ -15,10 +15,15 @@ public class LoginController {
 	@RequestMapping(method = GET)
 	public String login(
 			Model model,
-			@RequestParam(value = "logout",	required = false) String logout
+			@RequestParam(value = "logout",	required = false) String logout,
+			@RequestParam(value = "failure",	required = false) String login
 		){
 		if(logout != null){
 			model.addAttribute("logoutMessage", "You have been successfully logged out");
+		}
+		
+		if(login != null){
+			model.addAttribute("failureMessage", "Login failed, check your nick and password");
 		}
 		
 		return "loginPage";
